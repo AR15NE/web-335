4bab4241029e6ff9a777b3ef571905059ff02316
Amanda Rovey <155773970+AR15NE@users.noreply.github.com>
2024-08-24 09:58:42 -0500

    Exercise 2.3 Python
    Pushing all changes for week 2 exercise 2.3 python in Web335.

---
+++ week_2/pythonexercise.py
@@ -0,0 +1,14 @@
+""" 
+    Title: pythonexercise.py
+    Author: Amanda Rovey
+    Date: Aug 24 2024
+    Description: This file outputs a quote and its author
+"""
+
+#Quote and Author variables, stored as strings
+
+quote = "You have heard that it was said, 'Eye for eye, and tooth for tooth.' But I tell you, do not resist an evil person. If anyone slaps you on the right cheek, turn to them the other cheek also. And if anyone wants to sue you and take your shirt, hand over your coat as well. If anyone forces you to go one mile, go with them two miles. Give to the one who asks you, and do not turn away from the one who wants to borrow from you."
+author = "Jesus Christ"
+
+# Using the print function to output the quote and author
+print(author, 'said "', quote, '"')

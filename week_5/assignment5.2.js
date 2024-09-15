471d71a6ee4781c2eb876d112fe3b62a45f2e301
Amanda Rovey <155773970+AR15NE@users.noreply.github.com>
2024-09-14 19:47:21 -0500

    Assignment 5.2
    Pushing all changes for week 5 Assignment 5.2 Projections.

---
+++ week_5/assignment5.2.js
@@ -0,0 +1,32 @@
+// Adding a new user
+db.users.insertOne({
+  firstName: "Antonio",
+  lastName: "Vivaldi",
+  employeeId: "1013",
+  email: "avivaldi@me.com",
+  dateCreated: new Date()
+})
+
+// Verifying the new user was added
+db.users.find({ email: "avivaldi@me.com" })
+
+// Updating Mozart's email address
+db.users.updateOne(
+  { firstName: "Wolfgang", lastName: "Mozart" },
+  { $set: { email: "mozart@me.com" } }
+)
+
+// Verifying the update
+db.users.find({ firstName: "Wolfgang", lastName: "Mozart" })
+
+// Updating Mozart's email address
+db.users.updateOne(
+  { firstName: "Wolfgang", lastName: "Mozart" },
+  { $set: { email: "mozart@me.com" } }
+)
+
+// Verifying the update
+db.users.find({ firstName: "Wolfgang", lastName: "Mozart" })
+
+// Displaying all users with projections
+db.users.find({}, { _id: 0, firstName: 1, lastName: 1, email: 1 })
